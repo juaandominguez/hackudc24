42ab599e5b3e6c1b0b916ec2cbe4dfd0b06e21e9
Javier Miguel Sánchez <javier.miguel@udc.es>
2024-02-17 15:46:09 +0100

    button for create new schemas

--- components/main.tsx
+++ components/main.tsx
@@ -28,7 +28,7 @@ const Main = async () => {
                     </Link>
                 ))}
             </div>
-        </>
+            </>
     );
 };
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,11 +3,16 @@ import LoggedAvatar from './avatars/logged-avatar'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import UnloggedAvatar from './avatars/unlogged-avatar'
+import Link from 'next/link'
+import { Button } from './ui/button'
 const Navbar = async () => {
     const supabase = createServerComponentClient({ cookies })
     const { data: { user }, error } = await supabase.auth.getUser()
     return (
-        <nav className='flex justify-end mx-auto w-[50vw] mt-6'>
+        <nav className='flex justify-between md:mx-auto md:w-[50vw] mt-6 items-center mx-14'>
+            <Link href="/newschema">
+                <Button variant={"secondary"}>Create New Schema</Button>
+            </Link>
             {user ? <LoggedAvatar user={user} /> : <UnloggedAvatar />}
         </nav>
     )
